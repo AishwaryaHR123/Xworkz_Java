ab96f78bc52a80acb398e17dde971aa2eef7b2b5
AishwaryaHR123 <aishwaryahr.xworkz@gmail.com>
2023-07-23 18:40:10 +0530

    Static & Instance Variables

---
+++ Brick.java
@@ -0,0 +1,33 @@
+class Brick{
+	static String type;
+	static String brand;
+	String size;
+	String color;
+	Brick(String size,String color)
+	{
+		System.out.println("invoking String,String in Brick");
+		this.size=size;
+		this.color=color;
+	}
+	static
+	{
+		type="clay Bricks";
+		brand="Bharat";
+	}
+	static void printStatic()
+	{
+		System.out.println("invoking printStatic in Brick");
+		System.out.println("The Type is:"+type);
+		System.out.println("The brand name is:"+brand);
+	}
+	void printInstance()
+	{
+		System.out.println("invoking printInstance in Brick");
+		System.out.println("The Size is:"+this.size);
+		System.out.println("The brick color is:"+this.color);
+	}
+}
+		
+	
+	
+	
