3c25d8f5e092269697d9e0b618861e8f151afa71
AishwaryaHR123 <aishwaryahr.xworkz@gmail.com>
2023-07-23 18:52:53 +0530

    static and instance variables

---
+++ BrickKiller.java
@@ -0,0 +1,9 @@
+class BrickKiller{
+	public static void main(String[] args)
+	{
+		System.out.println("Running main in BrickKiller");
+		Brick brick=new Brick("Modular","Red");
+		brick.printStatic();
+		brick.printInstance();
+	}
+}
