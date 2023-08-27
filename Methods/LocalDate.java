3721e6bb0cef923558113ff28f350a935f0fcf71
AishwaryaHR123 <140000342+AishwaryaHR123@users.noreply.github.com>
2023-08-27 22:15:54 +0530

    Add files via upload

---
+++ Methods/LocalDate.java
@@ -0,0 +1,16 @@
+package com.xworkz.app.Methods;
+
+public class LocalDate {
+
+	public static void main(String[] args) {
+		System.out.println("Invoking main in LocalDate");
+		LocalDate localDate = new LocalDate();
+		System.out.println(localDate.equals(localDate));
+		System.out.println(localDate.getClass());
+		System.out.println(localDate.hashCode());
+		System.out.println(localDate.toString());
+		//System.out.println(localDate.notify());
+
+	}
+
+}
